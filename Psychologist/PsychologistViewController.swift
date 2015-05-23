ec58b71c8dddf85fbfce4bd57aeae53e511756a3
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-23 12:32:17 +0200

    Initial commit

---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PsychologistViewController.swift
+//  Psychologist
+//
+//  Created by Mads Bielefeldt on 23/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
